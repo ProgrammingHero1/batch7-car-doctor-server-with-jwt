dba008059be0ab7c162c3e36a75b5e3d9ef3bb6f
Programming Hero <programming.hero1@gmail.com>
2023-05-09 18:07:55 -0500

    save bookings to the database

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
         await client.connect();
 
         const serviceCollection = client.db('carDoctor').collection('services');
+        const bookingCollection = client.db('carDoctor').collection('bookings');
 
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
@@ -48,6 +49,16 @@ async function run() {
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
         })
+
+
+        // bookings 
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        });
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
