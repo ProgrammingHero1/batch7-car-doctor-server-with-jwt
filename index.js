9c0c9d3ae4f9d2d531825fff712b0d3421ac9b87
Programming Hero <programming.hero1@gmail.com>
2023-05-09 09:01:02 -0500

    added services reading api

--- index.js
+++ index.js
@@ -27,12 +27,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('carDoctor').collection('services');
+
+    app.get('/services', async(req, res) =>{
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
