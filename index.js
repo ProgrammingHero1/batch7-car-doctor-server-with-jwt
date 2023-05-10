a736bbb6abe5eb27df6f4a2884f3c6fd57e152fe
Programming Hero <programming.hero1@gmail.com>
2023-05-09 21:45:52 -0500

    created delete api to delete a booking

--- index.js
+++ index.js
@@ -69,6 +69,18 @@ async function run() {
             res.send(result);
         });
 
+        app.put('/bookings/:id', async(req, res) => {
+            const updatedBooking = req.body;
+            
+        })
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
