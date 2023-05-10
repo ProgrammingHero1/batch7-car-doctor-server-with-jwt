75c57f619ee5df68ce27e20c2ea61f6f7337686b
Programming Hero <programming.hero1@gmail.com>
2023-05-10 10:12:41 -0500

    added jwt token route to generate access token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -31,6 +32,16 @@ async function run() {
         const serviceCollection = client.db('carDoctor').collection('services');
         const bookingCollection = client.db('carDoctor').collection('bookings');
 
+        // jwt
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            console.log(token);
+            res.send({token});
+        })
+
+        // services routes
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -51,7 +62,7 @@ async function run() {
         })
 
 
-        // bookings 
+        // bookings routes
         app.get('/bookings', async (req, res) => {
             console.log(req.query.email);
             let query = {};
