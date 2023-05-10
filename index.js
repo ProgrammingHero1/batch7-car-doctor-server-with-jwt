7534a4be9382dbaeee0ef31cebb64b013925404c
Programming Hero <programming.hero1@gmail.com>
2023-05-10 11:26:44 -0500

    recap jwt verify method

--- index.js
+++ index.js
@@ -24,24 +24,21 @@ const client = new MongoClient(uri, {
     }
 });
 
-
-const verifyJWT = (req, res, next) =>{
-    console.log('hitting verify JWT')
-    console.log(req.headers.authorization);
+const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
     if(!authorization){
-        return res.status(401).send({error: true, message: 'unauthorized access'})
+        return res.status(401).send({error: true, message: 'unauthorized access'});
     }
     const token = authorization.split(' ')[1];
-    console.log('token inside Verify JWT', token);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-        if(error){
-            return res.status(403).send({error: true, message: 'unauthorized access'})
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+            return res.status(401).send({error: true, message: 'unauthorized access'})
         }
         req.decoded = decoded;
         next();
-    } )
+    })
 }
+
 
 async function run() {
     try {
@@ -83,7 +80,13 @@ async function run() {
 
         // bookings routes
         app.get('/bookings', verifyJWT, async (req, res) => {
-            console.log('came back after verify')
+            const decoded = req.decoded;
+            console.log('came back after verify', decoded)
+
+            if(decoded.email !== req.query.email){
+                return res.status(403).send({error: 1, message: 'forbidden access'})
+            }
+
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
